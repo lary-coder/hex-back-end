76ce07b630f8b47fcf2f9c416c0d404f75549f58
Laryssa Ribeiro <laryssarib@gmail.com>
2022-01-12 15:48:11 -0300

    6-my_github.py

---
+++ 0x01-python-network_1/6-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Python script that fetches id from api.github.com/users """
+
+import requests as res
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        r = res.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+        print(r.json().get('id'))
+    except BaseException:
+        pass
